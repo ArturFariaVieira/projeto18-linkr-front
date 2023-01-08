221e0065e50c7388a5a5c6a29468626a2c9fae89
duuda2011@gmail.com <elissonalvessilva@MacBook-Pro-de-Elisson.local>
2023-01-08 18:47:08 -0300

    feat: adicionado component de post na timeline

--- src/pages/Timeline.js
+++ src/pages/Timeline.js
@@ -1,9 +1,11 @@
 import Post from "../components/Post.js";
+import Publish from "../components/Publish.js";
 
 export default function Timeline() {
   return(
     <>
       <Post />
+      <Publish/>
     </>
   )
 }
