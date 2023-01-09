e474a2fe86764f331182c22a457e492d21508a8a
duuda2011@gmail.com <elissonalvessilva@MacBook-Pro-de-Elisson.local>
2023-01-09 16:55:37 -0300

    reafct:ajustes

--- src/pages/Timeline.js
+++ src/pages/Timeline.js
@@ -1,6 +1,6 @@
 import Post from "../components/Post.js";
 import Publish from "../components/Publish.js";
-
+import LinkPreview from "../components/LinkPreview.js";
 export default function Timeline() {
   return(
     <>
